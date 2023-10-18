ca6946596b0baae6342b377d4ce81798b4fd3d55
hyewon-han <hw1635@hanmail.net>
2023-10-18 22:07:11 +0900

    카드 불러오기 성공!

--- js/api.js
+++ js/api.js
@@ -13,14 +13,41 @@ fetch(
 )
   .then((response) => response.json())
   .then((response) => {
-    console.log(response.results["0"].poster_path);
-    const base_url = "https://image.tmdb.org/";
-    const file_size = "t/p/w500";
-    const file_path = response.results["0"].poster_path;
+    console.log(response);
+    const result = response.results;
+    // const base_url = "https://image.tmdb.org/";
+    // const file_size = "t/p/w500";
+    // const file_path = result["0"].poster_path;
+    // const movieTitle = document.querySelector(".movie-card__title");
+    // const moviePoster = document.querySelector(".movie-card__poster");
 
-    const movieTitle = document.querySelector(".movie-card__title");
-    const moviePoster = document.querySelector(".movie-card__poster");
-    movieTitle.innerText = response.results["0"].title;
-    moviePoster.src = `${base_url}${file_size}${file_path}`;
+    // movieTitle.innerText = result["0"].title;
+    // moviePoster.src = `${base_url}${file_size}${file_path}`;
+
+    // addMovie(result);
+    result.forEach((element) => {
+      console.log(element.title);
+      addMovie(element);
+    });
   })
   .catch((err) => console.error(err));
+
+const movieCards = document.querySelector(".movie-cards");
+
+function addMovie(element) {
+  const base_url = "https://image.tmdb.org/";
+  const file_size = "t/p/w500";
+  const file_path = element.poster_path;
+  const div = document.createElement("div");
+  div.classList.add("movie-card");
+
+  div.innerHTML = `<div>
+  <img src=${base_url}${file_size}${file_path} class="movie-card__poster" />
+</div>
+<div class="movie-card__title">
+  <span class="movie-card__title">${element.title}</span>
+</div>
+<div class="movie-card__overview">${element.overview}</div>
+<div class="movie-card__vote-average">${element.vote_average}</div>`;
+  movieCards.append(div);
+}
