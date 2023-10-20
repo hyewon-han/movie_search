586680d2297cc47edcef23cefae5b9c03dd4f58a
hyewon-han <hw1635@hanmail.net>
2023-10-20 16:19:48 +0900

    주석 삭제

--- js/api.js
+++ js/api.js
@@ -43,23 +43,3 @@ function addMovie(movie) {
   div.id = movie.id;
   movieCards.append(div);
 }
-
-// function searchMovie() {
-//   const movieCard = movieCards.querySelectorAll(".movie-card");
-//   const searchValue = document.getElementById("search__value");
-//   const searchBtn = document.querySelector(".search__btn");
-
-//   function handleSearch(e) {
-//     e.preventDefault();
-//     let value = searchValue.value;
-//     movieCard.forEach((element) => {
-//       element.classList.remove("hidden");
-//       let movieTitle = element.childNodes[2].childNodes[1].innerText;
-
-//       if (!movieTitle.toLowerCase().includes(value.toLowerCase())) {
-//         element.classList.add("hidden");
-//       }
-//     });
-//   }
-//   searchBtn.addEventListener("click", handleSearch);
-// }
