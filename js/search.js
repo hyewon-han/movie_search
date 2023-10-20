576b32ec6270d16e6b9c99ef96aa349ec691fdc9
hyewon-han <hw1635@hanmail.net>
2023-10-20 16:12:49 +0900

    search.js 파일 분리 / input value 대소문자 구별 가능

--- js/search.js
+++ js/search.js
@@ -1,9 +1,21 @@
-// const searchValue = document.getElementById("search__value");
-// const searchBtn = document.querySelector(".search__btn");
+const movieCards = document.querySelector(".movie-cards");
 
-// function handleSearch(e) {
-//   e.preventDefault();
-//   console.log(searchValue.value);
-// }
+export function searchMovie() {
+  const movieCard = movieCards.querySelectorAll(".movie-card");
+  const searchValue = document.getElementById("search__value");
+  const searchBtn = document.querySelector(".search__btn");
 
-// searchBtn.addEventListener("click", handleSearch);
+  function handleSearch(e) {
+    e.preventDefault();
+    let value = searchValue.value;
+    movieCard.forEach((element) => {
+      element.classList.remove("hidden");
+      let movieTitle = element.childNodes[2].childNodes[1].innerText;
+
+      if (!movieTitle.toLowerCase().includes(value.toLowerCase())) {
+        element.classList.add("hidden");
+      }
+    });
+  }
+  searchBtn.addEventListener("click", handleSearch);
+}
